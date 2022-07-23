5ac1f209cd467593ab54fe1938a31b4c793f3219
jia-han <neo.jiahan1234@gmail.com>
2022-07-23 15:52:55 +0800

    code cleanup for login-related screens

--- front_end/test/widget_test.dart
+++ front_end/test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:front_end/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget( MyApp());
 
